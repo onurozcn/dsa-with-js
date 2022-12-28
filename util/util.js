62172d3a6b584ec19f5b209a84f9cbb04532d9e9
Onur Ozcan <ozcan-onur@hotmail.com>
2022-12-28 17:33:54 +0100

    Add helpers

---
+++ util/util.js
@@ -0,0 +1,18 @@
+const Compare = {
+  LESS_THAN: -1,
+  BIGGER_THAN: 1,
+  EQUALS: 0,
+};
+
+function defaultCompare(a, b) {
+  if (a === b) {
+    return Compare.EQUALS;
+  }
+  return a < b ? Compare.LESS_THAN : Compare.BIGGER_THAN;
+}
+
+ function defaultEquals(a, b) {
+  return a === b;
+}
+
+module.exports = {defaultEquals, defaultCompare, Compare}
