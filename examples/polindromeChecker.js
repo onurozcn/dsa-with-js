7a51382391fd4c781881b5c32a1c1873598bdf53
Onur Ozcan <ozcan-onur@hotmail.com>
2023-01-03 23:17:00 +0100

    Add recursive solution. Fix bug on deque solution

--- examples/polindromeChecker.js
+++ examples/polindromeChecker.js
@@ -20,6 +20,7 @@ function palindromeChecker(aString) {
    
     firstChar = deque.removeFront(); 
     lastChar = deque.removeBack(); 
+    if(!lastChar) return isEqual
     if (firstChar !== lastChar) {
       isEqual = false; 
     }
@@ -27,5 +28,19 @@ function palindromeChecker(aString) {
   return isEqual;
 }
 
+// recursive version
+function isPolindrome(str) {
+  const lowerCaseStr = str.toLowerCase().split(" ").join("");
+  if (lowerCaseStr.length <= 1) return true;
+  if (lowerCaseStr[0] === lowerCaseStr.slice(-1))
+    return isPolindrome(lowerCaseStr.slice(1, -1));
+  else return false;
+}
 
 console.log(palindromeChecker("123442231415164354"))
+console.log(isPolindrome("123442231415164354"));
+
+
+
+console.log(palindromeChecker("akaka"));
+console.log(isPolindrome("akaka"));
